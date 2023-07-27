46beb878c155ddcb53b94cdf3a801ac9167474f5
Mikhail-Pakhomov <121101457+Mikhail-Pakhomov@users.noreply.github.com>
2023-07-27 17:02:40 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication28.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <deque>
+
+int main() {
+	std::deque<int> sequence = { -13, 0, 10, -4, 6, 11, 12, 28, 50, 89, 166, 304 };
+	const int desiredSize = 48;
+
+    while (sequence.size() < desiredSize) {
+        // Розширення вліво
+        int leftExtend = sequence[2] - sequence[1] - sequence[0] + 1;
+        sequence.push_front(leftExtend);
+
+        // Розширення вправо
+        int rightExtend = sequence[sequence.size() - 3] + sequence[sequence.size() - 2] + sequence[sequence.size() - 1] - 1;
+        sequence.push_back(rightExtend);
+    }
+
+    // Виведення всіх елементів послідовності
+    for (int num : sequence) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
